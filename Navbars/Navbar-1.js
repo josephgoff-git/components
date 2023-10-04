0025360be9e80809ab4dd9dc4851135404ff7448
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-04 17:41:31 -0400

    Create Navbar-1.js

---
+++ Navbars/Navbar-1.js
@@ -0,0 +1,10 @@
+  function Navbar() {
+    return (
+      <div style={{backgroundColor: "black", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
+          <p>Home</p>
+          <p>About</p>
+          <p>Contact</p>
+          <p>Products</p>
+      </div>
+    );
+  }
