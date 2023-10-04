88d16c895b346a9102c942be81751a5b07883bd7
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-04 18:34:55 -0400

    Create Navbar2.js

---
+++ Navbars/NavBar2/Navbar2.js
@@ -0,0 +1,12 @@
+function Navbar2() {
+  return (
+    <div style={{backgroundColor: "black", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <p>Home</p>
+      <p>Products</p>
+      <p>Contact</p>
+      <p>About</p>
+    </div>
+  );
+}
+
+export default Navbar2;
