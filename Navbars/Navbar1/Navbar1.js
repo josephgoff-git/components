85d8d5070ec26ccdf4c829b5d1f31cf7ae0d5789
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-06 04:44:45 -0400

    Update Navbar1.js

--- Navbars/Navbar1/Navbar1.js
+++ Navbars/Navbar1/Navbar1.js
@@ -1,6 +1,6 @@
 function Navbar1() {
   return (
-    <div style={{backgroundColor: "black", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <div style={{backgroundColor: "white", color: "black", borderBottom: "1px solid black", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", gap: "15px"}}>
       <p>Home</p>
       <p>About</p>
       <p>Contact</p>
