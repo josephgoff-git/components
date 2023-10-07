b553f5672bb62b7a13f485dd1e939d81b3dcefb7
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-06 23:14:26 -0400

    Update Navbar2.js

--- Navbars/Navbar2/Navbar2.js
+++ Navbars/Navbar2/Navbar2.js
@@ -1,6 +1,6 @@
 function Navbar2() {
   return (
-    <div style={{backgroundColor: "black", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <div style={{backgroundColor: "red", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
       <p>Home</p>
       <p>Products</p>
       <p>Contact</p>
