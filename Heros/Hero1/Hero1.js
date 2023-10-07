a083329093d8cf7bb4eabe307e2d65b637371189
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-06 23:39:27 -0400

    Create Hero1.js

---
+++ Heros/Hero1/Hero1.js
@@ -0,0 +1,9 @@
+function Hero1() {
+  return (
+    <div style={{backgroundColor: "white", color: "gray", borderBottom: "1px solid black", height: "100px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", gap: "15px"}}>
+        <p>This is fucking cool hehe</p>
+    </div>
+  );
+}
+
+export default Hero1;
