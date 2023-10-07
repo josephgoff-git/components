4239f876bcbd575ddd67c6403be390cc8ea48b55
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-06 23:36:15 -0400

    Update Navbar3.js

--- Navbars/Navbar3/Navbar3.js
+++ Navbars/Navbar3/Navbar3.js
@@ -1,1 +1,12 @@
-Hi
+function Navbar3() {
+  return (
+    <div style={{backgroundColor: "blue", color: "black", borderBottom: "1px solid black", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", gap: "15px"}}>
+      <p>Home</p>
+      <p>About</p>
+      <p>Contact</p>
+      <p>Products</p>
+    </div>
+  );
+}
+
+export default Navbar3;
