770d2bb713d1dcc56bd72e0b39c94ffa047360ca
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-07 17:30:34 -0400

    Update Hero1.js

--- Heros/Hero1/Hero1.js
+++ Heros/Hero1/Hero1.js
@@ -1,6 +1,6 @@
 function Hero1() {
   return (
-    <div style={{backgroundColor: "white", color: "gray", borderBottom: "1px solid black", height: "100px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", gap: "15px"}}>
+    <div style={{backgroundColor: "white", color: "gray", borderBottom: "1px solid black", height: "100px", width: "100%", display: "flex", justifyContent: "center", alignItems: "center", gap: "15px"}}>
         <p>This is fucking cool hehe</p>
     </div>
   );
