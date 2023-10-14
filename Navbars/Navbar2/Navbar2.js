5d9a956e44feb6f14e796c9165512344f02c1fe3
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-14 14:29:28 -0400

    Update Navbar2.js

--- Navbars/Navbar2/Navbar2.js
+++ Navbars/Navbar2/Navbar2.js
@@ -1,11 +1,21 @@
-function Navbar2() {
+import './Navbar2.css';
+
+const Navbar2 = () => {
   return (
-    <div style={{backgroundColor: "red", height: "60px", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <p>Home</p>
-      <p>Products</p>
-      <p>Contact</p>
-      <p>About</p>
-    </div>
+    <nav className="navbar">
+      <div className="logo-container">
+        <img
+          src="https://images.unsplash.com/photo-1633409361618-c73427e4e206?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJyYW5kfGVufDB8fDB8fHww&auto=format&fit=crop&w=900&q=60" alt="Logo" className="logo" />
+
+      </div>
+      <ul className="nav-list">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#services">Our Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
   );
 }
 
