53f294e3888950627e8d2de8143435af57433071
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-14 14:36:41 -0400

    Update Navbar1.js

--- Navbars/Navbar1/Navbar1.js
+++ Navbars/Navbar1/Navbar1.js
@@ -3,7 +3,7 @@ import './Navbar1.css';
 const Navbar1 = () => {
   return (
     <nav className="navbar1">
-      <div className="logo">Logo</div>
+      <div className="logo1">Logo</div>
       <ul className="nav1-list">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
