f94a227c1dd86d249f1240070c4dfa5149d1b214
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-11-03 23:48:51 -0400

    Create Hero1.js

---
+++ Heros/Hero1/Hero1.js
@@ -0,0 +1,17 @@
+// Hero1.js
+import React from 'react';
+import './Hero1.css';
+
+const Hero1 = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1 className="hero-title">Your Professional Work</h1>
+        <p className="hero-subtitle">A brief description of what you do</p>
+        <button className="hero-button">Learn More</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero1;
