7f23e07602d281716b8754becbfc885a6a537e4e
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-11-03 23:58:00 -0400

    Update Hero2.js

--- Heros/Hero2/Hero2.js
+++ Heros/Hero2/Hero2.js
@@ -7,7 +7,7 @@ const Hero2 = () => {
       <div className="hero2-content">
         <h1 className="hero2-title">Your Stunning Title</h1>
         <p className="hero2-subtitle">A Subtitle That Grabs Attention</p>
-        <button className="hero2-button">Explore Now</button>
+        <button className="hero2-button button">Explore Now</button>
       </div>
     </div>
   );
