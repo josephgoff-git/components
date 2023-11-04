00ea3cb4c1b3f61f2f5a8d25420a93962cd8a347
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-11-03 23:56:03 -0400

    Create Hero2.js

---
+++ Heros/Hero2/Hero2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero2.css';
+
+const Hero2 = () => {
+  return (
+    <div className="hero2-container">
+      <div className="hero2-content">
+        <h1 className="hero2-title">Your Stunning Title</h1>
+        <p className="hero2-subtitle">A Subtitle That Grabs Attention</p>
+        <button className="hero2-button">Explore Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero2;
