30dfe08f632fb63394b8d37adeb39d08cca462bc
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-11-16 13:49:40 -0500

    Update Display2.js

--- Displays/Display2/Display2.js
+++ Displays/Display2/Display2.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './Display1.css';
+import './Display2.css';
 
-const Display1 = () => {
+const Display2 = () => {
   return (
     <div className="full-width-image">
       <img src="https://images.unsplash.com/photo-1695753648663-72cc4dd99449?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Full Width Image" />
@@ -9,4 +9,4 @@ const Display1 = () => {
   );
 };
 
-export default Display1;
+export default Display2;
