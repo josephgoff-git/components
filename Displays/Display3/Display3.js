56b1a569dc3b385f88cf8ad6ff87a528cbb9dc6b
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-11-16 13:59:57 -0500

    Create Display3.js

---
+++ Displays/Display3/Display3.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Display3.css';
+
+const Display3 = () => {
+  return (
+    <div className="display3">
+      <div className="image-container">
+        <img src="https://images.unsplash.com/photo-1492950103599-127d2be251b7?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fGZsb3dlcnN8ZW58MHwxfDB8fHww" alt="Image 1" />
+      </div>
+      <div className="image-container">
+        <img src="https://images.unsplash.com/photo-1615280825886-fa817c0a06cc?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGZsb3dlcnN8ZW58MHwxfDB8fHww" alt="Image 2" />
+      </div>
+    </div>
+  );
+};
+
+export default Display3;
