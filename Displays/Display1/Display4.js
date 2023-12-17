f394617c862ad82c039aa96dcba78997081d5870
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-12-16 16:22:21 -0800

    Update and rename Display1.js to Display4.js

--- Displays/Display1/Display4.js
+++ Displays/Display1/Display4.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import './Display1.css';
+import './Display4.css';
 
-const Display1 = () => {
+const Display4 = () => {
   return (
-    <div className="display1-image-gallery">
-      <div className="display1-image-container">
+    <div className="display4-image-gallery">
+      <div className="display4-image-container">
         <img src="https://images.unsplash.com/photo-1682695795931-a546abdb6733?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image 1" />
       </div>
-      <div className="display1-image-container">
+      <div className="display4-image-container">
         <img src="https://images.unsplash.com/photo-1700076823924-a87fca414724?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8" alt="Image 2" />
       </div>
-      <div className="display1-image-container">
+      <div className="display4-image-container">
         <img src="https://images.unsplash.com/photo-1682687982204-f1a77dcc3067?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHx8" alt="Image 3" />
       </div>
     </div>
   );
 };
 
-export default Display1;
+export default Display4;
