6d7ca9f476d1588be0f478e113bbd006b562de65
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-12-18 14:02:29 -0800

    Update Image1.js

--- Images/Image1/Image1.js
+++ Images/Image1/Image1.js
@@ -3,7 +3,7 @@ import './Image1.css';
 
 const Image1 = () => {
   return (
-    <div className="image2-full-width-image">
+    <div className="image1-full-width-image">
       <img src="https://images.unsplash.com/photo-1695753648663-72cc4dd99449?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Full Width Image" />
     </div>
   );
