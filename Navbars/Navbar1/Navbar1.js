74f7674d187dfba75cd497213612bafd8d4363ce
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-12-18 14:37:23 -0800

    Create Navbar1.js

---
+++ Navbars/Navbar1/Navbar1.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { FaBars, FaHome, FaUser, FaEnvelope } from 'react-icons/fa';
+import { GrTechnology } from "react-icons/gr";
+import { MdSpaceDashboard } from "react-icons/md";
+import './Navbar1.css';
+
+const Navbar1 = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className="navbar1-container">
+      <div className="navbar1-left">
+        <div className="navbar1-logo">
+          <FaHome size={24}/>
+        </div>
+        <div className="navbar1-title">Company</div>
+      </div>
+      <div className="navbar1-menu-button" onClick={handleToggleMenu}>
+        <FaBars size={24} />
+      </div>
+      <div className={`navbar1-links ${showMenu ? 'show-menu' : ''}`}>
+        <a href="#" className="navbar1-link">
+          <FaUser size={16} />
+          About
+        </a>
+        <a href="#" className="navbar1-link">
+          <GrTechnology size={16} />
+          Products
+        </a>
+        <a href="#" className="navbar1-link">
+          <MdSpaceDashboard size={16} />
+          Dashboard
+        </a>
+            <a href="#" className="navbar1-link">
+          <FaEnvelope size={16} />
+          Contact
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar1;
